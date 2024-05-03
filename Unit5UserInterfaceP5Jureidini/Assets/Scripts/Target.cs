5c068669eb81a0eeefa2dd0140ca8718303e760a
MrLongarms <145394433+MrLongarms@users.noreply.github.com>
2024-05-03 00:58:40 -0700

    5.3 Step 4 Done!

--- Unit5UserInterfaceP5Jureidini/Assets/Scripts/Target.cs
+++ Unit5UserInterfaceP5Jureidini/Assets/Scripts/Target.cs
@@ -10,7 +10,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -3;
+    private float ySpawnPos = -1;
 
     public ParticleSystem explosionParticle;
     public int pointValue;
@@ -38,14 +38,21 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     Vector3 RandomForce()
